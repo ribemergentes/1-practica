55f76397689ddb817c2da469080ef45bb9fa7553
ribemergentes <magribaldgithub@gmail.com>
2024-10-16 16:36:17 -0400

    pagina terminado

--- app.py
+++ app.py
@@ -8,9 +8,22 @@ app = Flask(__name__)
 def inicio():
     return render_template("inicio.html")
 
-@app.route("/contactos")
+
+
+
+
+@app.route("/contactos", methods=['GET', 'POST'])
 def contactos():
+    if request.method == 'POST':
+        # Recoger los datos del formulario
+        nombre = request.form.get("nombre")
+        correo = request.form.get("correo")
+        mensaje = request.form.get("mensaje")
+        # Renderizar la página de salida con los datos proporcionados
+        return render_template("salidademensajes.html", nombre=nombre, correo=correo, mensaje=mensaje)
+    # Si es una petición GET, renderizar el formulario de contacto
     return render_template("contactos.html")
+
 
 @app.route("/noticias")
 def noticias():
